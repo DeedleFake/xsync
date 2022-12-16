7736bb1ae6f2e4adfbb3b89303359ae47ae61974
DeedleFake <deedlefake@users.noreply.github.com>
2022-12-16 15:49:01 -0500

    cq: overhaul implementation

--- cq/cq.go
+++ cq/cq.go
@@ -3,39 +3,30 @@ package cq
 
 import "sync"
 
-// A BulkQueue queues up values of type T and yields a collection of
-// them all at once as a value of type Q.
-type BulkQueue[T, Q any] struct {
+// A Queue concurrently collects values and returns them in FIFO
+// order. A zero value Queue is ready to use.
+type Queue[T any] struct {
+	start sync.Once
+
 	done  chan struct{}
 	close sync.Once
 
-	add  chan T
-	get  chan Q
-	wrap func([]T) Q
+	add chan T
+	get chan T
 }
 
-// Simple creates a simple Queue that yields its values as a slice of
-// T.
-func Simple[T any]() *BulkQueue[T, []T] {
-	return New(func(q []T) []T { return q })
-}
+func (q *Queue[T]) init() {
+	q.start.Do(func() {
+		q.done = make(chan struct{})
+		q.add = make(chan T)
+		q.get = make(chan T)
 
-// New creates a Queue that converts queued up values into a Q by
-// calling collect.
-func New[T, Q any](collect func([]T) Q) *BulkQueue[T, Q] {
-	q := BulkQueue[T, Q]{
-		done: make(chan struct{}),
-		add:  make(chan T),
-		get:  make(chan Q),
-		wrap: collect,
-	}
-	go q.run()
-
-	return &q
+		go q.run()
+	})
 }
 
 // Stop stops the queue.
-func (q *BulkQueue[T, Q]) Stop() {
+func (q *Queue[T]) Stop() {
 	q.close.Do(func() {
 		close(q.done)
 	})
@@ -43,30 +34,26 @@ func (q *BulkQueue[T, Q]) Stop() {
 
 // Add returns a channel that enqueues values sent to it. This channel
 // must not be closed.
-func (q *BulkQueue[T, Q]) Add() chan<- T {
+func (q *Queue[T]) Add() chan<- T {
+	q.init()
 	return q.add
 }
 
-// Get returns a channel that yields all values enqueued since the
-// last time it was recieved from as a Q. In other words, if ints are
-// being queued
-//
-//	q.Add() <- 1
-//	v = <-q.Get() // [1]
-//	q.Add() <- 2
-//	q.Add() <- 3
-//	v = <-q.Get() // [2, 3]
-func (q *BulkQueue[T, Q]) Get() <-chan Q {
+// Get returns a channel that yields values from the queue when they
+// are available. The channel will be closed when the Queue is
+// stopped.
+func (q *Queue[T]) Get() <-chan T {
+	q.init()
 	return q.get
 }
 
-func (q *BulkQueue[T, Q]) run() {
+func (q *Queue[T]) run() {
 	defer func() {
 		close(q.get)
 	}()
 
-	var s []T
-	var get chan Q
+	var s list[T]
+	var get chan T
 
 	for {
 		select {
@@ -74,12 +61,14 @@ func (q *BulkQueue[T, Q]) run() {
 			return
 
 		case v := <-q.add:
-			s = append(s, v)
+			s.Enqueue(v)
 			get = q.get
 
-		case get <- q.wrap(s):
-			s = nil
-			get = nil
+		case get <- s.Peek():
+			ok := s.Pop()
+			if !ok {
+				get = nil
+			}
 		}
 	}
 }
