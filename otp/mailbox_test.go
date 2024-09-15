a85a8e6d8a6cac4a2353d2b100e78417d31c0d91
DeedleFake <deedlefake@users.noreply.github.com>
2024-09-15 19:46:53 -0400

    otp: add a concurrent test

--- otp/mailbox_test.go
+++ otp/mailbox_test.go
@@ -7,20 +7,50 @@ import (
 )
 
 func TestMailbox(t *testing.T) {
-	var mb otp.Mailbox
-	mb.Send(1)
-	mb.Send(2)
-	mb.Send(3)
-	v := otp.Recv(&mb, func(v int) bool { return v%2 == 0 })
-	if v != 2 {
-		t.Fatal(v)
-	}
-	v = otp.Recv[int](&mb, nil)
-	if v != 1 {
-		t.Fatal(v)
-	}
-	v = otp.Recv[int](&mb, nil)
-	if v != 3 {
-		t.Fatal(v)
-	}
+	t.Run("sequential", func(t *testing.T) {
+		var mb otp.Mailbox
+		mb.Send(1)
+		mb.Send(2)
+		mb.Send(3)
+		v := otp.Recv(&mb, func(v int) bool { return v%2 == 0 })
+		if v != 2 {
+			t.Fatal(v)
+		}
+		v = otp.Recv[int](&mb, nil)
+		if v != 1 {
+			t.Fatal(v)
+		}
+		v = otp.Recv[int](&mb, nil)
+		if v != 3 {
+			t.Fatal(v)
+		}
+	})
+
+	t.Run("concurrent", func(t *testing.T) {
+		var mb otp.Mailbox
+		c := make(chan int)
+		go func() {
+			c <- otp.Recv(&mb, func(v int) bool { return v%2 == 0 })
+		}()
+
+		go func() {
+			mb.Send(1)
+			mb.Send(2)
+			mb.Send(3)
+		}()
+		v := <-c
+		if v != 2 {
+			t.Fatal(v)
+		}
+
+		match := func(v int) bool { return v%2 != 0 }
+		v = otp.Recv(&mb, match)
+		if v != 1 {
+			t.Fatal(v)
+		}
+		v = otp.Recv(&mb, match)
+		if v != 3 {
+			t.Fatal(v)
+		}
+	})
 }
